fa44fff223940e50ec5a097176b9e3722c9c8e3e
Bastian Aron Kramer <bastian4444@live.dk>
2023-01-03 13:09:51 +0100

    Domed down for ipcore

---
+++ SourceFiles/DumedDown/test.c
@@ -0,0 +1,9 @@
+// NeuralNetwork.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include "network.cpp"
+#include "InputImages.h"
+
+
+int main() 
+{
+	printf("%d",forward(test_arrayz));
+}
