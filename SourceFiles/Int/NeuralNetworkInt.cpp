5a9782bd8e0509c180064caaea9c932f9d9e132d
Bastian Aron Kramer <bastian4444@live.dk>
2023-01-03 12:26:22 +0100

    int now

--- SourceFiles/Int/NeuralNetworkInt.cpp
+++ SourceFiles/Int/NeuralNetworkInt.cpp
@@ -1,8 +1,18 @@
 // NeuralNetwork.cpp : This file contains the 'main' function. Program execution begins and ends there.
-#include "NeuralNetworkInt.hpp"
+#include "InputImages.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <algorithm>
 
-int NeuralNetwork(float img_array[numInputs]) {
-	return Network::Instance().forward(img_array);
 
+int main() 
+{
+	printf("static int test_arrayz[784] = {");
+	for (int i = 0; i < 784; i++) {
+		printf("%d, ", static_cast<int>(test_arrayz[i] * 100));
+	}
+	printf("};");
 }
-
