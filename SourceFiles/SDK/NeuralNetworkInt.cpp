f921aad3cdf0ec904c757c18e8eec44af1c4e135
Bastian Aron Kramer <bastian4444@live.dk>
2023-01-05 14:14:23 +0100

    SDK code

--- SourceFiles/SDK/NeuralNetworkInt.cpp
+++ SourceFiles/SDK/NeuralNetworkInt.cpp
@@ -5,5 +5,24 @@
 
 int main() 
 {
-	printf("%d", Network::Instance().forward(test_array0));
+
+	printf("%d", Network::getInstance().image_process(test_array0));
+	printf("%d", Network::getInstance().image_process(test_array1));
+	printf("%d", Network::getInstance().image_process(test_array2));
+	printf("%d", Network::getInstance().image_process(test_array3));
+	printf("%d", Network::getInstance().image_process(test_array4));
+	printf("%d", Network::getInstance().image_process(test_array5));
+	printf("%d", Network::getInstance().image_process(test_array6));
+	printf("%d", Network::getInstance().image_process(test_array7));
+	printf("%d", Network::getInstance().image_process(test_array8));
+	printf("%d", Network::getInstance().image_process(test_array9));
+	printf("%d", Network::getInstance().image_process(test_arrayplus));
+	printf("%d", Network::getInstance().image_process(test_arrayminus));
+	printf("%d", Network::getInstance().image_process(test_arraydot));
+	printf("%d", Network::getInstance().image_process(test_arrayslash));
+	printf("%d", Network::getInstance().image_process(test_arrayw));
+	printf("%d", Network::getInstance().image_process(test_arrayx));
+	printf("%d", Network::getInstance().image_process(test_arrayy));
+	printf("%d", Network::getInstance().image_process(test_arrayz));
+	
 }
