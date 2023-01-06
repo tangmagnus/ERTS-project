2cf6e573b6dd16e7f7c5b6dcddcc7f94b9e6c6e0
Bastian Aron Kramer <bastian4444@live.dk>
2023-01-06 12:47:56 +0100

    Restucture and added int normal

---
+++ SourceFiles/Int_Normal/NeuralNetworkInt.cpp
@@ -0,0 +1,10 @@
+// NeuralNetwork.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include "InputImages.h"
+#include "networkInt.h"
+
+
+int main() 
+{
+	Network network = Network();
+	printf("%d", network.forward(test_array2));
+}
---
+++ SourceFiles/Int_Normal/networkInt.h
@@ -0,0 +1,73 @@
+#ifndef NETWORK 
+#define NETWORK_H
+
+//#include <systemc.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <algorithm>
+
+#include "ArrayDefinitionsInt.h"
+
+#define numInputs 784
+#define numHiddenNodes 30
+#define numOutputs 18
+
+
+class Network {
+private:
+
+public:
+
+    //network functions
+    //reLu activation function
+    int reLU(int x) {
+        return (x > 0) ? x : 0;
+    }
+    //void outActiviation_softMax();
+    int forward(int input_array[numInputs])
+    {
+        int hiddenLayer[numHiddenNodes];
+        int intOutput[numOutputs];
+        int sum1[numHiddenNodes];
+        int digitis = 100;
+
+        for (int i = 0; i < numHiddenNodes; i++)
+        {
+            sum1[i] = fc1_bias[i];
+            for (int j = 0; j < numInputs; j++)
+            {
+                sum1[i] += input_array[j] * fc1_weights[i][j];
+            }
+            hiddenLayer[i] = reLU(sum1[i]);
+        }
+
+
+        for (int i = 0; i < numOutputs; i++)
+        {
+            intOutput[i] = fc2_bias[i];
+            for (int j = 0; j < numHiddenNodes; j++)
+            {
+                intOutput[i] += hiddenLayer[j] * fc2_weights[i][j];
+            }
+        }
+
+
+        int element = 0;
+        for (int i = 0; i < numOutputs; i++)
+        {
+            if (intOutput[element] < intOutput[i])
+                element = i;
+        }
+
+        return element;
+    }
+
+};
+
+
+
+#endif 
+
